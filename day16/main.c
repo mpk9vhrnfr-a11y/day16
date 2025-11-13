c6e5e79c366706237186b180974ee7cf1967e738
sanaa <Sanaakumar07@gmail.com>
2025-11-13 12:30:52 +0530

    c

--- day16/main.c
+++ day16/main.c
@@ -1,15 +1,26 @@
-//
-//  main.c
-//  day16
-//
-//  Created by Sanaa Kumar on 02/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void) {
+    int num, binary[32], i = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num == 0) {
+        printf("Binary: 0\n");
+        return 0;
+    }
+
+    while (num > 0) {
+        binary[i] = num % 2;
+        num /= 2;
+        i++;
+    }
+
+    printf("Binary: ");
+    for (int j = i - 1; j >= 0; j--)
+        printf("%d", binary[j]);
+    printf("\n");
+
+    return 0;
 }
